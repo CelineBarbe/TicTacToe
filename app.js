9de094cfe2528d8a0bca6fdec6811ec2787381b6
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-11-01 17:29:48 +0100

    bouton pour recommencer la partie

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var gameBoard = document.getElementById('board');
 
-var nbCellules = 3 * 3;
+function initGame() {
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
+    }
+    var nbCellules = 3 * 3;
 for (var indexCell = 0; indexCell < nbCellules; indexCell++) {
     var cell = document.createElement('div');
     cell.classList.add('case');
     gameBoard.appendChild(cell);
     cell.addEventListener('click', onClick)
 }
+}
+
+initGame();
 
 var round = 0;
 
