449d5e331e08070693c7512c56d838cac85aced9
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-11-01 17:01:21 +0100

    version avec des couleurs, deux joueurs

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var gameBoard = document.getElementById('board');
 
-
-// Etape 1.2 - "Boucler" 8x8 fois
 var nbCellules = 3 * 3;
 for (var indexCell = 0; indexCell < nbCellules; indexCell++) {
-
-    // Etape 1.3 - Dans la boucles générer une cases
     var cell = document.createElement('div');
     cell.classList.add('case');
     gameBoard.appendChild(cell);
     cell.addEventListener('click', onClick)
 }
 
+var round = 0;
+
 function onClick(event) {
     var cell = event.target;
-    cell.classList.toggle('redcase')
+    if (round % 2 == 0) {
+        cell.classList.add('redcase');
+    } else {
+        cell.classList.add('bluecase');
+    };
+    round +=1;
+    console.log(round);
 }
